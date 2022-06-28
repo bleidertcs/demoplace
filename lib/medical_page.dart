b7d5458bbffe6bbea481e13c5633b3ea858d5a0d
nico-rojas229 <nicolas.rojas@tcsvzla.com>
2022-06-28 16:11:45 -0400

    cambios

--- lib/medical_page.dart
+++ lib/medical_page.dart
@@ -7,21 +7,23 @@ class MedicalPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Atencion Medica'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: Column(
-          children: [
-            ProductCard(
-              image: 'assets/botella.jpg',
-              productName: 'Botella',
-              productPrice: 'Bs. 10.000',
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('Atencion Medica'),
         ),
-      ),
+        body: gridView(context));
+  }
+
+  Widget gridView(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 2,
+      mainAxisSpacing: 20,
+      children: List.generate(10, (index) {
+        return ProductCard(
+          image: 'assets/botella.jpg',
+          productName: 'Botella',
+          productPrice: 'Bs. 10.000',
+        );
+      }),
     );
   }
 }
--- lib/pharmacy_home_page.dart
+++ lib/pharmacy_home_page.dart
@@ -29,112 +29,107 @@ class PharmacyPage extends StatelessWidget {
         children: [
           Form(
             key: key,
-            child: TextFormField(
-              textInputAction: TextInputAction.next,
-              decoration: InputDecoration(
-                focusedBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.black, width: 2),
-                  borderRadius: BorderRadius.circular(15),
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextFormField(
+                textInputAction: TextInputAction.next,
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.black, width: 2),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  suffixIcon: const Icon(Icons.search),
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 10),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.black, width: 2),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  errorStyle: const TextStyle(
+                    color: Colors.red,
+                    fontSize: 14,
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  hintText: 'Search',
+                  hintStyle: const TextStyle(color: Colors.black45),
                 ),
-                suffixIcon: const Icon(Icons.search),
-                contentPadding: const EdgeInsets.symmetric(horizontal: 10),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.black, width: 2),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                errorStyle: const TextStyle(
-                  color: Colors.red,
-                  fontSize: 14,
-                ),
-                border: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.white),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                hintText: 'Search',
-                hintStyle: const TextStyle(color: Colors.black45),
               ),
             ),
           ),
-          const Text(
-            'Categorias',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(80.0),
-                ),
-                child: const Text(
-                  'Todo',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
+          // const Text(
+          //   'Categorias',
+          //   style: TextStyle(
+          //     fontSize: 20,
+          //     fontWeight: FontWeight.bold,
+          //   ),
+          // ),
+          const SizedBox(height: 15),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(80.0),
+                  ),
+                  child: const Text(
+                    'Todo',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w900,
+                      fontSize: 18,
+                      color: Colors.black38,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(width: 5),
-              const Text(
-                'Cuidado Personal',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
+                const SizedBox(width: 10),
+                const Text(
+                  'Cuidado Personal',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w900,
+                    fontSize: 23,
+                    color: Color(0xff51b9cb),
+                  ),
                 ),
-              ),
-              const SizedBox(width: 5),
-              const Text(
-                'Dolor e imflamacion',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
+                const SizedBox(width: 10),
+                const Text(
+                  'Dolor ',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w900,
+                    fontSize: 18,
+                    color: Colors.black38,
+                  ),
                 ),
-              ),
-              const SizedBox(width: 5),
-              const Text(
-                'Pastillas',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                ),
-              ),
-            ],
+                const SizedBox(width: 5),
+                //const Text(
+                //  'Pastillas',
+                //  textAlign: TextAlign.center,
+                //  style: TextStyle(
+                //    fontWeight: FontWeight.bold,
+                //    fontSize: 16,
+                //  ),
+                //),
+              ],
+            ),
           ),
+          const SizedBox(height: 15),
           Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: const [
               ProductCard(
-                image: '',
+                image: 'assets/botella.jpg',
                 productName: '',
                 productPrice: '',
               ),
               ProductCard(
-                image: '',
-                productName: '',
-                productPrice: '',
-              ),
-            ],
-          ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              ProductCard(
-                image: '',
-                productName: '',
-                productPrice: '',
-              ),
-              ProductCard(
-                image: '',
+                image: 'assets/pharmacy1.jpg',
                 productName: '',
                 productPrice: '',
               ),
@@ -145,12 +140,12 @@ class PharmacyPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: const [
               ProductCard(
-                image: '',
+                image: 'assets/pharmacy2.jpg',
                 productName: '',
                 productPrice: '',
               ),
               ProductCard(
-                image: '',
+                image: 'assets/pharmacy1.jpg',
                 productName: '',
                 productPrice: '',
               ),
@@ -161,12 +156,28 @@ class PharmacyPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: const [
               ProductCard(
-                image: '',
+                image: 'assets/botella.jpg',
                 productName: '',
                 productPrice: '',
               ),
               ProductCard(
-                image: '',
+                image: 'assets/pharmacy2.jpg',
+                productName: '',
+                productPrice: '',
+              ),
+            ],
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              ProductCard(
+                image: 'assets/botella.jpg',
+                productName: '',
+                productPrice: '',
+              ),
+              ProductCard(
+                image: 'assets/botella.jpg',
                 productName: '',
                 productPrice: '',
               ),
